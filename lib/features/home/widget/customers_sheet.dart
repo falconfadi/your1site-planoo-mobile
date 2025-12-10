666ca3356b9d7ca944cb507ec5625a0e5f590e3e
MAYA-SKEF <maya@your1site.com>
2025-12-10 20:26:53 +0300

    updates in customers sheet

--- lib/features/home/widget/customers_sheet.dart
+++ lib/features/home/widget/customers_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:centro_partner/core/constants/app_styles.dart';
+import 'package:centro_partner/core/ui/shared_widgets/expandable_text_widget.dart';
 import 'package:centro_partner/core/ui/widgets/cached_image.dart';
 import 'package:centro_partner/features/home/data/model/customer_model.dart';
 import 'package:centro_partner/features/home/data/model/review_model.dart';
@@ -36,26 +37,41 @@ class _CustomersSheetState extends State<CustomersSheet> {
                 color: AppColors.lightGrayColor,
                 borderRadius: BorderRadius.circular(10.r),
               ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  CachedImage(
-                    width: 45.w,
-                    height: 45.w,
-                    imageUrl: widget.forReview == false ?
-                    widget.reviews![index].customer!.profileImage == null ? "" :  widget.reviews![index].customer!.profileImage!.url! :
-                    widget.customers[index].profileImage == null ? "" :  widget.customers[index].profileImage!.url!,
-                    fit: BoxFit.cover,
-                    borderColor: AppColors.grayColor,
-                    borderRadius: 40.r,
-                    borderWidth: 1,
-                    errorForUser: true,
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CachedImage(
+                        width: 45.w,
+                        height: 45.w,
+                        imageUrl: widget.forReview == false ?
+                        widget.reviews![index].customer!.profileImage == null ? "" :  widget.reviews![index].customer!.profileImage!.url! :
+                        widget.customers[index].profileImage == null ? "" :  widget.customers[index].profileImage!.url!,
+                        fit: BoxFit.cover,
+                        borderColor: AppColors.grayColor,
+                        borderRadius: 40.r,
+                        borderWidth: 1,
+                        errorForUser: true,
+                      ),
+                      SizedBox(width: 10.w),
+                      Expanded(child: Text(widget.forReview == false ?
+                      widget.reviews![index].customer!.name! :
+                      widget.customers[index].name!,style: AppTheme.headlineSmall.copyWith(color: AppColors.primaryColor))),
+                    ],
                   ),
-                  SizedBox(width: 10.w),
-                  Expanded(child: Text(widget.forReview == false ?
-                  widget.reviews![index].customer!.name! :
-                  widget.customers[index].name!,style: AppTheme.headlineSmall.copyWith(color: AppColors.primaryColor))),
+                  SizedBox(height: widget.forReview == true ? 0 : 10.h),
+                  widget.forReview == true ? Center() :
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.w),
+                    child: ExpandableTextWidget(
+                        text: widget.reviews![index].content!,
+                        style: AppTheme.bodyLarge.copyWith(fontSize: 18.sp)
+                    ),
+                  )
+
                 ],
               ),
             );
